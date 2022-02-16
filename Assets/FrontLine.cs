fb5ff33d785f83a9ebcf8b04ec1f485e23dda200
Michael <babaukos@rambler.ru>
2022-02-16 21:10:51 +0200

    Add files

---
+++ Assets/FrontLine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class FrontLine : MonoBehaviour 
+{
+	public LineRenderer line1;
+	public LineRenderer line2;
+	public List<FrontLinePoint> points;
+	
+	// Update is called once per frame (60 times per second)
+	private void Update () 
+	{
+		if(points == null)
+			return;
+
+		UpdatePoints ();
+	}
+	// Update is called 50 times per second
+	private void UpdatePoints () 
+	{
+		line1.positionCount = points.Count;
+		line2.positionCount = points.Count;
+		for (int i = 0; i < points.Count; i++)
+		{
+			line1.SetPosition(i, points[i].point1);
+
+			int p = (points.Count - 1) - i;
+			line2.SetPosition(i, points[p].point2);
+		}
+		// for (int i = points.Count-1; i >= 0; i--)
+		// {
+			
+		// }
+	}
+}
---
+++ Assets/FrontLinePoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class FrontLinePoint : MonoBehaviour 
+{
+	[SerializeField] private float offset = 2;
+	public Vector3 point1
+	{
+		get
+		{
+			return transform.position + transform.forward * offset;
+		}
+	}
+	public Vector3 point2
+	{
+		get
+		{
+			return transform.position - transform.forward * offset;
+		}
+	}
+
+#if UNITY_EDITOR
+	private void OnDrawGizmos() 
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(point1, point2);
+		Gizmos.DrawSphere(transform.position, 0.06f);
+	}
+#endif
+}
